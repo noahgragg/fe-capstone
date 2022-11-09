8e6935f8d5105a838a3ee9fbfdbee5263b17dbfe
Lowjoejoe <joseph.t.low@gmail.com>
2022-11-09 14:13:54 -0800

    completed initial dataSeed.sql

--- back-end/dataServer/dataSeed.sql
+++ back-end/dataServer/dataSeed.sql
@@ -3,9 +3,13 @@ VALUES
 
 ("Lowjoe", "Joe", "Low", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL", "No resume uploaded", "https://github.com/Lowjoejoe"),
 ("noahgragg", "Noah", "Gragg", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL. code focused on World of Warcraft", "No resume uploaded", "https://github.com/noahgragg"),
-("noahgragg", "Noah", "Gragg", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL. code focused on World of Warcraft", "No resume uploaded", "https://github.com/noahgragg");
+("JpKelley", "Jeff", "Kelley", "junior software engineer 2nd of his name, 1st of the dependants with experience with JavaScript, HTML, CSS, React.js, postgreSQL. code focused on World of Warcraft", "No resume uploaded", "https://github.com/Jpkelley6");
 
 INSERT INTO projects (project_name, project_link, project_desc)
 VALUES 
 
-()
+("React-MVP-Circuit-Workouts", "https://github.com/Lowjoejoe/React-MVP-Circuit-Workouts","allows users to build custom circuit workouts",1),
+("mvp-fullstack", "https://github.com/noahgragg/mvp-fullstack", "displays loot and bosses from Ice Crown Citadel.",2),
+("React-MVP", "https://github.com/Jpkelley6/React-MVP","Diablo II builds finder", 3),
+("Shoe_Shop", "https://github.com/Lowjoejoe/Shoe_Shop","online shoe shop with full CRUD", 1);
+
