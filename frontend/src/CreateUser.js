74d7b8eb8984a97d891078379dd68444a67dba19
Bwade808 <bwka85@gmail.com>
2022-11-09 16:15:28 -0700

    added routes and links

---
+++ frontend/src/CreateUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateUser = () => {
+  return (
+    <div>
+      <h1>CREATE USER PAGE</h1>
+    </div>
+  )
+}
+
+export default CreateUser
---
+++ frontend/src/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+    <div>
+      <h1>LOGIN PAGE</h1>
+    </div>
+  )
+}
+
+export default LogIn
