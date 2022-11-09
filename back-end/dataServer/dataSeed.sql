7f10777f91b201a31ca4128fa855ca58421957ca
Lowjoejoe <joseph.t.low@gmail.com>
2022-11-09 14:02:59 -0800

    dataSeed.sql initial update

---
+++ back-end/dataServer/dataSeed.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (username, first_name, last_name, summary, resume_link, github_link) 
+VALUES
+
+("Lowjoe", "Joe", "Low", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL", "No resume uploaded", "https://github.com/Lowjoejoe"),
+("noahgragg", "Noah", "Gragg", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL. code focused on World of Warcraft", "No resume uploaded", "https://github.com/noahgragg"),
+("noahgragg", "Noah", "Gragg", "junior software engineer with experience with JavaScript, HTML, CSS, React.js, postgreSQL. code focused on World of Warcraft", "No resume uploaded", "https://github.com/noahgragg");
+
+INSERT INTO projects (project_name, project_link, project_desc)
+VALUES 
+
+()
