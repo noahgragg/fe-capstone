5406067a9151be30b5bb9119c4c715012c2321af
Bwade808 <bwka85@gmail.com>
2022-11-10 14:47:37 -0700

    add profile cards and additional css styling on home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,31 @@ import Home from './Home';
 import CreateUser from './CreateUser';
 import LogIn from './LogIn';
 
+const url = 'http://localhost:8000';
 const App = () => {
+  const [profileCardInfo, setProfileCardInfo] = useState([{
+    user_id: 0,
+    username: '',
+    first_name: '',
+    last_name: '',
+    summary: '',
+    resume_link: '',
+    github_link: ''
+  }]);
+  useEffect(() => {
+      fetch(`${url}/`)
+      .then(res => res.json())
+      .then((data) => setProfileCardInfo(data)) 
+      console.log(profileCardInfo); 
+  }, [])
+
   return (
     <div className="App">
       <Header />
+      <div className='below-header'></div>
       <div>
         <Routes>
-          <Route path='/home' element={<Home />} />
+          <Route path='/home' element={<Home profileCardInfo={profileCardInfo}/>} />
           <Route path='/create-user' element={<CreateUser />} />
         </Routes>
       </div>
