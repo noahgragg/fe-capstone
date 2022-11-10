4ab93c6aa22ac1a129dae7c219c3ef82585bb642
Bwade808 <bwka85@gmail.com>
2022-11-10 09:40:47 -0700

    added more  css styling

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <div className='header-container'>
         <div className='nav-bar'>
             <div className='nav-bar-home'>
-                <Link id='nav-link' to='./home'>Home</Link> 
+                <Link id='nav-link' to='./home'><FaHome /></Link> 
             </div>
             <div className='nav-bar-login'>
                 <Link id='nav-link' to='./create-user'>Create User</Link>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>HOME PAGE</h1>
+    <div className='home-container'>
+        <div className='home-body'>
+            <h1 className='home-body-h1'>Welcome</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora itaque 
+                cupiditate velit. Non neque quibusdam nihil totam corrupti porro nam eveniet
+                nemo mollitia autem. Dicta ab vel veritatis pariatur numquam blanditiis 
+                minus, est hic necessitatibus fugiat quam saepe enim molestias repellat 
+                omnis unde debitis quia ea, rem cupiditate sapiente. Deserunt.
+            </p><br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque 
+                praesentium explicabo dignissimos rerum et similique cumque, deleniti, 
+                asperiores iste dolore, nostrum velit? Corporis enim saepe quidem repellat
+                 libero eum ipsam. Iusto repellendus eos mollitia, repudiandae eius 
+                 perferendis quidem minus. Voluptatem magni amet aliquid alias porro nobis impedit 
+                 quo minus doloribus minima, labore neque quisquam numquam facere sit, quod ratione 
+                 iusto omnis commodi perspiciatis, possimus placeat velit quibusdam laborum. 
+                 Molestiae expedita quo exercitationem, ex animi dolorum provident quisquam 
+                 quidem consequuntur totam laboriosam hic ipsum dolor sit. At corrupti dolorum 
+                 accusantium laborum eaque magnam laboriosam sit officia repellendus. Velit 
+                 nihil alias ipsam dicta explicabo consequatur repellendus quam dolores neque 
+                 odit. Perferendis aliquid repellendus, fugiat minus, non dignissimos eius 
+                 ducimus distinctio blanditiis aperiam at minima quae. Nobis itaque veniam sequi, 
+                 ipsa minima sed nam, quis adipisci enim fugit doloremque architecto eveniet 
+                 numquam facilis, tempora tenetur repellat consequatur hic similique fugiat reiciendis 
+                 saepe. Fugit delectus molestiae facere laboriosam magni vitae quibusdam. Adipisci ea 
+                 saepe autem laboriosam ipsa beatae omnis aliquid possimus eos porro ab dignissimos, 
+                 corporis sed corrupti, soluta quia deleniti delectus temporibus sint laudantium itaque 
+                 vitae accusantium similique. Ipsa eum assumenda eius non ad fuga maxime perspiciatis 
+                 temporibus sed obcaecati! Voluptas, dicta molestias.
+            </p>
+        </div>
     </div>
   )
 }
