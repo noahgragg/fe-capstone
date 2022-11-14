031e7d26df0a3674f5c361a81ad6d59317da0963
HAliUsm <h.usmanisiaka@gmail.com>
2022-11-14 15:43:59 -0600

    login modal complete

--- frontend/src/LogIn.js
+++ frontend/src/LogIn.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { Row, Col, Form } from 'react-bootstrap';
+import { FaEyeSlash, FaEye } from 'react-icons/fa';
+
 
 function LogIn(props) {
   const [show, setShow] = useState(false);
@@ -21,48 +24,61 @@ function LogIn(props) {
     }
 
   return (
-   <>
-        <Button variant='outline-dark' onClick={handleShow}>Login</Button>
+    <>
+      <Button variant='outline-light' onClick={handleShow}>Login</Button>
 
-        <Modal 
+      <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <input
-             className='username' 
-             placeholder='Username...'
-             type='text'
-             onChange={e => props.setUsername(e.target.value)}
-             />
-            <input
-             className='password'
-             placeholder='Password...'
-             type={passwordType}
-             onChange={handlePasswordChange}
-             value={passwordInput}
-             />
-             <button 
-             className='btn btn-outline-primary'
-             onClick={togglePassword}>
-              { passwordType ==="password" ? <i className="bi bi-eye-slash"></i> : <i className='bi bi-eye'></i>}
-             </button>
-          </Modal.Body>
-          <Modal.Footer>
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row>
+            <Col sm={6}>
+              <Form>
+                <Form.Group controlId='login'>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    className='username'
+                    placeholder='Username...'
+                    type='text'
+                    onChange={e => props.setUsername(e.target.value)}
+                  />
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    className='password'
+                    placeholder='Password...'
+                    type={passwordType}
+                    onChange={handlePasswordChange}
+                    value={passwordInput}
+                  /> 
+                </Form.Group>
+                </Form>
+                  <button
+                    className='btn btn-outline-primary'
+                    onClick={togglePassword}>
+                     {passwordType === "password" ? <FaEye />: <FaEyeSlash />  }
+                   </button>
+            </Col>
+          </Row>
+
+          {/* i className="fas fa-eye-slash"></i> : <i className='fas fa-eye'></i> */}
+        </Modal.Body>
+        <Modal.Footer>
           <Button variant="outline-dark">Login</Button>
           <Button variant="outline-secondary" onClick={handleClose}>
             Close
           </Button>
-          </Modal.Footer>
-        </Modal>
-   </>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
+
 
 export default LogIn
 
