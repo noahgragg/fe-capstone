40260a7b2b0f7f8dc9fc6f9d318ebe10aa6ea390
Lowjoejoe <joseph.t.low@gmail.com>
2022-11-14 09:31:34 -0800

    added modal login having issues with render to bottom of page

--- frontend/src/LogIn.js
+++ frontend/src/LogIn.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap-modal';
 
-const LogIn = () => {
+function LogIn(props) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true);
+
+    const [passwordType, setPasswordType] = useState("password");
+    const [passwordInput, setPasswordInput] = useState("")
+    const handlePasswordChange = (evnt) =>{
+      setPasswordInput(evnt.target.value)
+    }
+    const togglePassword = ()=> {
+      if(passwordType=== "password"){
+        setPasswordType("text")
+        return;
+      }
+      setPasswordType("password")
+    }
+
   return (
-    <div>
-      <h1>LOGIN PAGE</h1>
-    </div>
+   <>
+        <Button variant='outline-dark' onClick={handleShow}>Login</Button>
+
+        <Modal 
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input
+             className='username' 
+             placeholder='Username...'
+             type='text'
+             onChange={e => props.setUsername(e.target.value)}
+             />
+            <input
+             className='password'
+             placeholder='Password...'
+             type={passwordType}
+             onChange={handlePasswordChange}
+             value={passwordInput}
+             />
+             <button 
+             className='btn btn-outline-primary'
+             onClick={togglePassword}>
+              { passwordType ==="password" ? <i className="bi bi-eye-slash"></i> : <i className='bi bi-eye'></i>}
+             </button>
+          </Modal.Body>
+          <Modal.Footer>
+          <Button variant="outline-dark">Login</Button>
+          <Button variant="outline-secondary" onClick={handleClose}>
+            Close
+          </Button>
+          </Modal.Footer>
+        </Modal>
+   </>
   )
 }
 
 export default LogIn
+
+
