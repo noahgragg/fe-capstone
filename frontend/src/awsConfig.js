c638164224ec753dbdd3b730e11ab0d2498966e0
noahgragg <105750729+noahgragg@users.noreply.github.com>
2022-11-16 17:11:22 -0600

    Delete awsConfig.js

--- frontend/src/awsConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    accessKeyId: 'AKIAW6O7GIQI5FVDBEXC', 
-    secretAccessKey: '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',
-    bucket: 'fe-capstone-bucket',
-    region: 'us-east-2',
-    bucketURL: 'https://fe-capstone-bucket.s3.us-east-2.amazonaws.com/'
-}
