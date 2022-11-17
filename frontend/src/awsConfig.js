48115ff0016d27127ff34df5c1224842cd17c489
Lowjoejoe <joseph.t.low@gmail.com>
2022-11-17 09:34:37 -0800

    fixed gitignore for AWS

---
+++ frontend/src/awsConfig.js
@@ -0,0 +1,7 @@
+module.exports ={	
+	accessKeyId: '',
+	secretAccessKey: '',
+	bucket: 'fe-capstone-bucket',
+	region: 'us-east-2',
+	bucketURL: 'https://fe-capstone-bucket.s3.us-east-2.amazonaws.com/'
+	}
