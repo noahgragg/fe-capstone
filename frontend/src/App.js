185ffecbcc4dbf2642ab34eda1c7e1dea2eda22a
noahgragg <noah.gragg@gmail.com>
2022-11-19 16:49:25 -0600

    env variables workaround

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ const App = () => {
   useEffect(()=>{
     fetch(`${dataURL}/keys`)
       .then(res => res.json())
-      .then((results) => console.log(results))
+      .then((results) => setKeys(results))
   }, [])
 
   // useEffect(() => {
